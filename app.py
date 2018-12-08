66b89f0c6d0df904e9df0ad2e2fceb0f34065b95
Kanapong Siriprachai <42939643+Kana-P@users.noreply.github.com>
2018-12-08 20:41:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 """GUN VIOLENCE DATA"""
 import csv
-def counting_crime():
-    """Count number of crimes that happened in each year and add them to dictionary."""
+def gun_violence():
+    """Create the required variables to use in another function and call them here."""
     gun_data = open("gun-violence-data_01-2013_03-2018.csv", encoding="utf8")
     data = csv.reader(gun_data)
-    crime = [row for row in data]
+    case = [row for row in data]
+    case_count = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0, "2018": 0}
+    all_case(case_count, case)
+def all_case(data_set, case):
+    """Count number of case that happened in each year and add them to dictionary."""
     count_set = [0, 0, 0, 0, 0, 0]
-    crimes_count = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0, "2018": 0}
-    for crime_data in range(1, len(crimes)):
-        date = crimes[crime_data][1]
+    for case_data in range(1, len(case)):
+        date = case[crime_data][1]
         if date[:4] == "2013":
             count_set[0] += 1
-            crimes_count["2013"] = count_set[0]
+            data_set["2013"] = count_set[0]
         elif date[:4] == "2014":
             count_set[1] += 1
-            crimes_count["2014"] = count_set[1]
+            data_set["2014"] = count_set[1]
         elif date[:4] == "2015":
             count_set[2] += 1
-            crimes_count["2015"] = count_set[2]
+            data_set["2015"] = count_set[2]
         elif date[:4] == "2016":
             count_set[3] += 1
-            crimes_count["2016"] = count_set[3]
+            data_set["2016"] = count_set[3]
         elif date[:4] == "2017":
             count_set[4] += 1
-            crimes_count["2017"] = count_set[4]
+            data_set["2017"] = count_set[4]
         elif date[:4] == "2018":
             count_set[5] += 1
-            crimes_count["2018"] = count_set[5]
-            
+            data_set["2018"] = count_set[5]
+    print(data_set)
+    print("Total case: %d"%sum(count_set))
+gun_violence()
