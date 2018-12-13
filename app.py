2e813687ffc620b7c4ceedbc445cf925cfd9e3ba
Kanapong Siriprachai <42939643+Kana-P@users.noreply.github.com>
2018-12-13 23:42:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ def gun_violence():
     gun_data = open("gun-violence-data_01-2013_03-2018_edited.csv", encoding="utf8")
     read = csv.reader(gun_data)
     case = [row for row in read]
-    case_count = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0}
-    case_with_victim = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0}
-    victim = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0,}
-    jan_mar = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0, "2018": 0}
-    total_case = [0, 0, 0, 0, 0]
-    total_case_with_victim = [0, 0, 0, 0, 0]
-    total_victim = [0, 0, 0, 0, 0]
-    case_in_3_month = [0, 0, 0, 0, 0, 0]
+    case_count = {"2014": 0, "2015": 0, "2016": 0, "2017": 0}
+    case_with_victim = {"2014": 0, "2015": 0, "2016": 0, "2017": 0}
+    victim = {"2014": 0, "2015": 0, "2016": 0, "2017": 0,}
+    jan_mar = {"2014": 0, "2015": 0, "2016": 0, "2017": 0, "2018": 0}
+    total_case = [0, 0, 0, 0]
+    total_case_with_victim = [0, 0, 0, 0]
+    total_victim = [0, 0, 0, 0]
+    case_in_3_month = [0, 0, 0, 0, 0]
     all_case(case_count, total_case, case)
     with_victim(case_with_victim, total_case_with_victim, case)
     all_victim(victim, total_victim, case)
@@ -27,79 +27,70 @@ def gun_violence():
     graph(jan_mar, "case in 3 months", case_in_3_month, "Case in 3 months", "-yx")
 
 def all_case(data_set, count, case):
-    """Count number of case that happened from 1st January 2013 to 
+    """Count number of case that happened from 1st January 2014 to 
     31th December 2017 and add them to dictionary."""
     for case_data in range(1, len(case)):
         date = case[case_data][0].split("/")
-        if date[2] == "2013":
+        if date[2] == "2014":
             count[0] += 1
-            data_set["2013"] = count[0]
-        elif date[2] == "2014":
-            count[1] += 1
-            data_set["2014"] = count[1]
+            data_set["2014"] = count[0]
         elif date[2] == "2015":
-            count[2] += 1
-            data_set["2015"] = count[2]
+            count[1] += 1
+            data_set["2015"] = count[1]
         elif date[2] == "2016":
-            count[3] += 1
-            data_set["2016"] = count[3]
+            count[2] += 1
+            data_set["2016"] = count[2]
         elif date[2] == "2017":
-            count[4] += 1
-            data_set["2017"] = count[4]
+            count[3] += 1
+            data_set["2017"] = count[3]
     return data_set
     return sum(count)
     
 def with_victim(data_set, count, case):
     """
-    Count case with deaths and injured from 1st January 2013 to 
+    Count case with deaths and injured from 1st January 2014 to 
     31th December 2017 and add them to dictionary.
     """ 
     for case_data in range(1, len(case)):
         date = case[case_data][0].split("/")
         death = int(case[case_data][1])
         injured = int(case[case_data][2])
-        if date[2] == "2013" and death+injured > 0:
+        if date[2] == "2014" and death+injured > 0:
             count[0] += 1
-            data_set["2013"] = count[0]
-        elif date[2] == "2014" and death+injured > 0:
-            count[1] += 1
-            data_set["2014"] = count[1]
+            data_set["2014"] = count[0]
         elif date[2] == "2015" and death+injured > 0:
-            count[2] += 1
-            data_set["2015"] = count[2]
+            count[1] += 1
+            data_set["2015"] = count[1]
         elif date[2] == "2016" and death+injured > 0:
-            count[3] += 1
-            data_set["2016"] = count[3]
+            count[2] += 1
+            data_set["2016"] = count[2]
         elif date[2] == "2017" and death+injured > 0:
-            count[4] += 1
-            data_set["2017"] = count[4]
+            count[3] += 1
+            data_set["2017"] = count[3]
     return data_set
     return sum(count)
 
 def all_victim(data_set, count, case):
     """
-    Count all deaths and injured from 1st January 2013 to 
+    Count all deaths and injured from 1st January 2014 to 
     31th December 2017 and add them to dictionary.
     """
     for case_data in range(1, len(case)):
         date = case[case_data][0].split("/")
         death = int(case[case_data][1])
         injured = int(case[case_data][2])
-        if date[2] == "2013":
+        if date[2] == "2014":
             count[0] += death+injured
-            data_set["2013"] = count[0]
-        elif date[2] == "2014":
-            count[1] += death+injured
-            data_set["2014"] = count[1]
+            data_set["2014"] = count[0]
         elif date[2] == "2015":
-            count[2] += death+injured
-            data_set["2015"] = count[2]
+            count[1] += death+injured
+            data_set["2015"] = count[1]
         elif date[2] == "2016":
-            count[3] += death+injured
-            data_set["2016"] = count[3]
+            count[2] += death+injured
+            data_set["2016"] = count[2]
         elif date[2] == "2017":
-            count[4] += death+injured
-            data_set["2017"] = count[4]
+            count[3] += death+injured
+            data_set["2017"] = count[3]
     return data_set
     return sum(count)
 
@@ -110,24 +101,21 @@ def three_month(data_set, count, case):
     """
     for case_data in range(1, len(case)):
         date = case[case_data][0].split("/")
-        if date[2] == "2013" and int(date[1]) <= 3:
+        if date[2] == "2014" and int(date[1]) <= 3:
             count[0] += 1
-            data_set["2013"] = count[0]
-        elif date[2] == "2014" and int(date[1]) <= 3:
-            count[1] += 1
-            data_set["2014"] = count[1]
+            data_set["2014"] = count[0]
         elif date[2] == "2015" and int(date[1]) <= 3:
-            count[2] += 1
-            data_set["2015"] = count[2]
+            count[1] += 1
+            data_set["2015"] = count[1]
         elif date[2] == "2016" and int(date[1]) <= 3:
-            count[3] += 1
-            data_set["2016"] = count[3]
+            count[2] += 1
+            data_set["2016"] = count[2]
         elif date[2] == "2017" and int(date[1]) <= 3:
-            count[4] += 1
-            data_set["2017"] = count[4]
+            count[3] += 1
+            data_set["2017"] = count[3]
         elif date[2] == "2018" and int(date[1]) <= 3:
-            count[5] += 1
-            data_set["2018"] = count[5]
+            count[4] += 1
+            data_set["2018"] = count[4]
     return data_set
     return sum(count)
 
@@ -135,12 +123,12 @@ def graph(data, data_type, total, y_axis, style):
     """
     Draw graph from stored data
     """
-    if len(data) < 6:
-        x = ["2013", "2014", "2015", "2016", "2017"]
-        y = [data["2013"], data["2014"], data["2015"], data["2016"], data["2017"]]
+    if len(data) < 5:
+        x = ["2014", "2015", "2016", "2017"]
+        y = [data["2014"], data["2015"], data["2016"], data["2017"]]
     else:
-        x = ["2013", "2014","2015", "2016", "2017", "2018"]
-        y = [data["2013"], data["2014"], data["2015"], data["2016"], data["2017"], data["2018"]]
+        x = ["2014","2015", "2016", "2017", "2018"]
+        y = [data["2014"], data["2015"], data["2016"], data["2017"], data["2018"]]
     plt.plot(x, y, style)
     plt.xlabel("Year")
     plt.ylabel(y_axis)
